b616ed93f230634868b68f926eac8cabed0862a2
HsinDiehKimChang <200397845@student.georgianc.on.ca>
2019-09-26 13:25:38 -0400

    20190926_lab2 update

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -16,10 +16,14 @@ test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
 
 # Make a report on what we have in the Pictures directory
-# test -d ~/Pictures && cat <<EOF
-# Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
-# The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
-# EOF
+test -d ~/Pictures && cat <<EOF
+Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
+The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
+EOF
+
+
+#Improve script
+echo "=========Improve script============="
 
 
 # Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
