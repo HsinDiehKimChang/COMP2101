a1de3a0a3cb18860ee6e794d093fc82fb750f895
HsinDiehKimChang <200397845@student.georgianc.on.ca>
2019-11-15 08:19:40 -0500

    lab2

---
+++ Powershell/geeknumbers.ps1
@@ -0,0 +1,9 @@
+﻿
+foreach ($c in (1..8)){
+    New-Object -TypeName psobject -Property @{
+        PlaceCount=$c;
+        MaxValueInBinary=[math]::pow(2,$c);
+        MaxValueInOctal=[math]::pow(8,$c);
+        MaxValueInHex=[math]::pow(16,$c);
+    }
+}
