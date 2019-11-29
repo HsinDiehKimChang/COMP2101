9b514205ad39f53355bd11ebc541c072965ccbbc
Kim <200397845@student.georgianc.on.ca>
2019-11-29 07:04:46 -0500

    Create lab4.ps1

---
+++ Powershell/lab4.ps1
@@ -0,0 +1,19 @@
+﻿
+
+#get-wmiobject -class win32_operatingsystem | Select-Object name,version |ft
+
+#get-wmiobject -class win32_computersystem | Select-Object description |ft
+
+
+
+
+get-wmiobject -class win32_processor  | Where-Object MaxClockSpeed -ne $null |
+format-table -autosize  @{Label="Speed"; Expression={$_.MaxClockSpeed}},
+ @{Label="Number of Core"; Expression={$_.NumberOfCores}},
+ @{Label="L2 CacheSize(MB)"; Expression={$_.L2CacheSize/1mb}},
+@{Label="L3 CacheSize(MB)"; Expression={$_.L3CacheSize/1mb}}
+
+
+
+
+#get-wmiobject -class win32_physicalmemory
