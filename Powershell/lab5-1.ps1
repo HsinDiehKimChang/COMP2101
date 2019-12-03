7fff87874667c621786b65741d99caddf6c7cc2d
Kim <200397845@student.georgianc.on.ca>
2019-12-02 20:02:30 -0500

    Create lab5-1.ps1

---
+++ Powershell/lab5-1.ps1
@@ -0,0 +1,7 @@
+﻿
+Param ([Parameter(Mandatory=$true,position=0,ValueFromPipeline=$true,
+ ValueFromPipelineByPropertyName=$true)][string]$ObjectName)
+
+"Object name was '$ObjectName'"
+$objTypeName = $ObjectName.gettype().name
+"$ObjectName type is $Objtypename object"
